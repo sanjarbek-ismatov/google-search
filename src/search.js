d2d2235218ea1322a5ac09de90cb1a711485c170
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-02-04 14:43:38 +0500

    .

--- src/search.js
+++ src/search.js
@@ -3,24 +3,19 @@ import './style/style.css';
 import Profile from './images/profile.jpg';
 import Logo from './images/logo.png';
 import Tia from './images/tia.png';
+import Google from './searchComponent/google';
 function Search(){
     const [search, setSearch] = useState();
     const [text, setText] = useState(false);
-    function fetchData(){
-        const CONTEXT_KEY = '344518b235096fb90';
-        const API_KEY = 'AIzaSyBbFGoK91L85xUu9ONymejA1ecQPAVN8gM';
-        let url = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${search}`;
-        if (search !== ''){
-            fetch(url)
-            .then(response => response.json())
-            .then(data => console.log(data))
-        }
-    }
+    
     const clearText = () => {
         return (
             <svg onClick={() => {
             }} className="clear" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
         )
+    }
+    const setProps = () => {
+        <Google text={search} />
     }
     return (
         <>
@@ -51,7 +46,9 @@ function Search(){
                         <svg className="searchIcon" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
                             <input type="text" onChange={({target}) => {
                                 if (target.value !== ""){
+                                    setSearch(target.value)
                                     setText(true)
+                                    setProps()
                                 }
                                 else{
                                     setText(false)
